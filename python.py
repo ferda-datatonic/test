691dd1bd7168c13755ab278f2d3050735fb96838
ferda-datatonic <ferda.ustun@datatonic.com>
2022-11-02 23:34:02 +0000

    c

--- python.py
+++ python.py
@@ -1,69 +1,45 @@
-nums =  [2,3,1,1,4]
-
-def jump(nums,pos):
-
-    if pos > len(nums)-1:
-        return False
-    
-    elif pos == len(nums)-1:
-        return True
-
-    for i in range(pos+1,(pos+nums[i]+1):
-        if jump(nums,pos):
-            return True
-    return False
-jump(nums,0)
+intervals = [[1,4],[4,5]]
 
 
+#it might make sense to go through the array first
+#and find the largest interval, first
 
+#we can keep going through the array and finding
+#the largest interval, merging all values that exist
+#within the scope of this parent interval, taking its min and
+#max values, create a new array with the result
+#continue for the remaining elements
 
+dummy = intervals.copy()
+def custom_sort(val):
+    return val[0]
+res = []
+while dummy != []:
+    largest = 0
+    index = 0
+    for idx,i in enumerate(dummy):
+        interval = abs(dummy[idx][0]-dummy[idx][1])
+        if interval>largest:
+            largest = interval
+            index = idx
+    temp = []
+    curr = dummy.pop(index)
+    temp.append(curr)
+    l = 0
+    while l <= len(dummy)-1 and dummy != []:
+        if (dummy[l][0] >= curr[0] and dummy[l][0]<=curr[1]) or (dummy[l][1]<=curr[1] and dummy[l][1]>=curr[0]):
+            temp.append(dummy[l])
+            dummy.pop(l)
+        else:
+            l+=1   
 
+    loc_min = 3497587
+    loc_max = -4938595
+    for results in temp:
+        if results[0]<loc_min:
+            loc_min = results[0]
+        if results[1]>loc_max:
+            loc_max = results[1]
+    res.append([loc_min,loc_max])
 
-
-
-
-
-
-
-# def can_jump_memoization(i, array, cache):
-
-#     if i >= len(array):
-#         return False
-#     elif i == len(array)-1:
-#         return True
-#     elif i in cache:
-#         return cache[i]
-#     else:
-#         cache[i] = False
-#         for j in range(1, array[i]+1):
-#             if can_jump_memoization(j+i, array, cache):
-#                 cache[i] = True
-#                 return True
-#         return cache[i]
-# print(can_jump_memoization(0,nums,{}))
-
-# def canJump(nums):
-#     N = len(nums)
-
-#     def dfs(i, memo):
-#         if i == N - 1:
-#             return True
-        
-#         if nums[i] == 0:
-#             return False
-        
-#         if memo[i] != None:
-#             return memo[i]
-        
-#         is_valid = False
-        
-#         for j in range(i + 1, (i+nums[i])+1):
-#             if dfs(j, memo):
-#                 is_valid = True
-#                 break
-        
-#         memo[i] = is_valid
-#         return memo[i]
-    
-#     return dfs(0, [None] * N)
-# print(canJump(nums))
+print(res)
