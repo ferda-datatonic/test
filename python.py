8168a91a3620dcde68c9b342c0e0b81e80c03ec6
ferda-datatonic <ferda.ustun@datatonic.com>
2022-11-03 18:09:03 +0000

    ugbyg

--- python.py
+++ python.py
@@ -1,45 +1,18 @@
-intervals = [[1,4],[4,5]]
+n = 3
+matrix = [[0]*n for _ in range(n)]
 
+def dfs(matrix,i,j,count):
+    if (i >= len(matrix)) or (i<0) or (j>=len(matrix[0])) or (j<0) or matrix[i][j] != 0:
+        return
 
-#it might make sense to go through the array first
-#and find the largest interval, first
-
-#we can keep going through the array and finding
-#the largest interval, merging all values that exist
-#within the scope of this parent interval, taking its min and
-#max values, create a new array with the result
-#continue for the remaining elements
-
-dummy = intervals.copy()
-def custom_sort(val):
-    return val[0]
-res = []
-while dummy != []:
-    largest = 0
-    index = 0
-    for idx,i in enumerate(dummy):
-        interval = abs(dummy[idx][0]-dummy[idx][1])
-        if interval>largest:
-            largest = interval
-            index = idx
-    temp = []
-    curr = dummy.pop(index)
-    temp.append(curr)
-    l = 0
-    while l <= len(dummy)-1 and dummy != []:
-        if (dummy[l][0] >= curr[0] and dummy[l][0]<=curr[1]) or (dummy[l][1]<=curr[1] and dummy[l][1]>=curr[0]):
-            temp.append(dummy[l])
-            dummy.pop(l)
-        else:
-            l+=1   
-
-    loc_min = 3497587
-    loc_max = -4938595
-    for results in temp:
-        if results[0]<loc_min:
-            loc_min = results[0]
-        if results[1]>loc_max:
-            loc_max = results[1]
-    res.append([loc_min,loc_max])
-
-print(res)
+    matrix[i][j] = count
+    count +=1
+    #in any case, we DO NOT want to go right
+    #until j+1 is within i
+    if j+1>=i:
+        dfs(matrix,i,j+1,count)
+    dfs(matrix,i+1,j,count)
+    dfs(matrix,i,j-1,count)
+    dfs(matrix,i-1,j,count)
+dfs(matrix,i=0,j=0,count=1)
+print(matrix)
