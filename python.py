d01f63ab769d7c83fa2f4844cd6585cfd90aa925
ferda-datatonic <ferda.ustun@datatonic.com>
2022-11-07 22:37:30 +0000

    sdgsdfg

--- python.py
+++ python.py
@@ -1,27 +1,34 @@
-def simplifyPath(self, path: str) -> str:
-    if path[-1] == "/":
-        path = path[:-1]
-    if path[0]!="/":
-        path = "/"+path 
-    indices = []
+class WordsAlphabet:
 
-    for i in range(len(path)):
-        if path[i] == "/":
-            indices.append(i)
-    path = list(path)
-    for i in range(len(indices[::-1])):
-        if i == len(indices)-1:
-            pass
-        else:
-            if abs(indices[i]-indices[i+1])==1:
-                path.pop(indices[i])
-    stack = []
-    while path != []:
-        curr = path.pop(0)
-        if stack == []:
-            stack.append(curr)
-        elif curr != "." and curr !="_":
-            stack.append(curr)
-        else:
-            continue
-    return "".join(stack)
+    def __init__(self, alphabet, fruit, country):
+        self.alphabet = alphabet
+        self.fruit = fruit
+        self.country = country
+
+    def __str__(self):
+        return "WordsAlphabet(alphabet:'%s', fruit='%s', country='%s')" % (self.alphabet, self.fruit, self.country)
+
+def apply_transforms(fruits, countries):
+    def map_to_alphabet_kv(word):
+        return (word[0], word)
+
+    def cogbk_result_to_wordsalphabet(cgbk_result):
+        (alphabet, words) = cgbk_result
+        return WordsAlphabet(alphabet, words['fruits'][0], words['countries'][0])
+
+    fruits_kv = (fruits | 'Fruit to KV' >> beam.Map(map_to_alphabet_kv))
+    countries_kv = (countries | 'Country to KV' >> beam.Map(map_to_alphabet_kv))
+
+    return ({'fruits': fruits_kv, 'countries': countries_kv}
+            | beam.CoGroupByKey()
+            | beam.Map(cogbk_result_to_wordsalphabet))
+
+
+
+with beam.Pipeline() as p:
+
+  fruits = p | 'Fruits' >> beam.Create(['apple', 'banana', 'cherry'])
+  countries = p | 'Countries' >> beam.Create(['australia', 'brazil', 'canada'])
+
+  (apply_transforms(fruits, countries)
+   | LogElements())
